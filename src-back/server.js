c27b76703866ed735582c9edbf4732a0a4365fd7
Nick Ng <nicholas.cj.ng@gmail.com>
2022-05-22 19:05:14 +1200

    wip. updated readme with path of exile state diagram

--- src-back/server.js
+++ src-back/server.js
@@ -2,14 +2,47 @@ import { readSettings } from "./settings.js";
 import { fetchCharacters } from "./ggg.js";
 import { makeTail } from "./log-watch.js";
 
+const sleep = (ms) =>
+  new Promise((resolve, _reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+
 const main = async () => {
   const settings = readSettings();
 
-  // const a = await fetchCharacters(settings);
+  let splits = [{ experience: 0, timestamp: 0, xphr: 0 }];
 
-  // console.log("a", a);
+  while (true) {
+    const a = await fetchCharacters(settings);
+    const b = a.filter((character) => character.lastActive);
 
-  makeTail("./test.txt");
+    if (b.length === 0) {
+      continue;
+    }
+
+    const character = b[0];
+    const newTimestamp = Date.now();
+
+    const changeInExperience = character.experience - splits[0].experience;
+    const changeInTime = newTimestamp - splits[0].timestamp;
+
+    const xphr = (changeInExperience / changeInTime) * 3600000;
+    console.log("XP", character.experience, "XP / hr", xphr);
+
+    splits.unshift({
+      experience: character.experience,
+      timestamp: newTimestamp,
+      xphr,
+    });
+
+    splits = splits.slice(0, 10);
+
+    await sleep(100000);
+  }
+
+  // makeTail("./test.txt");
 };
 
 main();
